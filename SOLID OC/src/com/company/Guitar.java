1f9b25e4d0f9a0ada67a02ab1535f6a3b091d259
BlindAspid <44471998+BlindAspid@users.noreply.github.com>
2021-04-08 10:56:52 +0300

    Add files via upload

---
+++ SOLID OC/src/com/company/Guitar.java
@@ -0,0 +1,9 @@
+package com.company.;
+
+public class Guitar {
+
+    private String make;
+    private String model;
+    private int volume;
+
+}
---
+++ SOLID OC/src/com/company/SuperGuitar.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class SuperGuitar extends Guitar {
+
+    private String flameColour;
+
+}
